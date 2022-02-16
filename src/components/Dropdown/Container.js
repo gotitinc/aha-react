9cc9fb6f2faad27ffaa11d3c30fcdcbb2bdb1484
Nguyễn Thanh Sơn <son@gotitapp.co>
2022-02-16 14:37:16 +0700

    refactor: Add proptypes docs & clean up Dropdown.Container

--- src/components/Dropdown/Container.js
+++ src/components/Dropdown/Container.js
@@ -6,21 +6,44 @@ import usePopper from 'hooks/usePopper';
 import useRootClose from 'hooks/useRootClose';
 import DropdownContext from './Context';
 
-
 const propTypes = {
+  /**
+   * Custom style
+   */
+  additionalStyles: PropTypes.object,
   /**
    * You can use a custom element type for this component.
    * @default div
-   * */
+   */
   as: PropTypes.elementType,
   /**
+   * Custom className
+   */
+  className: PropTypes.string,
+  /**
+   * Popper's `flip` modifier config.
+   * @see https://popper.js.org/docs/v2/modifiers/flip/
+   */
+  flip: PropTypes.boolean,
+  /**
    * A set of popper options and props passed directly to react-popper's Popper component.
+   * @default {}
    */
   popperConfig: PropTypes.object,
-  /** A `react-transition-group` Transition component used to animate the Message on dismissal. */
+  /**
+   * The DOM event name (click, mousedown, etc) that will close the dropdown
+   * @default click
+   */
+  rootCloseEvent: PropTypes.string,
+  /**
+   * Whether PopperJS should be used
+   * @default true
+   */
+  shouldUsePopper: PropTypes.bool,
+  /**
+   * A `react-transition-group` Transition component used to animate the Message on dismissal.
+   */
   transition: elementType,
-  /** Custom style  */
-  additionalStyles: PropTypes.object,
 };
 
 const defaultProps = {
@@ -28,12 +51,12 @@ const defaultProps = {
 
 const Container = React.forwardRef(({ additionalStyles, ...props }, ref) => {
   const {
-    className,
-    flip,
-    rootCloseEvent,
-    children,
-    popperConfig = {},
     as: Component = 'div',
+    flip,
+    children,
+    className,
+    popperConfig = {},
+    rootCloseEvent,
     shouldUsePopper = true,
     transition: Transition,
   } = props;
@@ -73,11 +96,11 @@ const Container = React.forwardRef(({ additionalStyles, ...props }, ref) => {
   const container = (
     <Component
       ref={ref}
-      {...containerProps}
+      {...containerProps} // eslint-disable-line react/jsx-props-no-spreading
       className={classNames(
         'Dropdown-container',
         'u-zIndexDropdownContainer u-backgroundWhite u-roundedMedium u-border u-positionAbsolute u-textLeft u-positionLeft u-marginVerticalTiny',
-        className && className
+        className && className,
       )}
     >
       {children}
@@ -85,6 +108,7 @@ const Container = React.forwardRef(({ additionalStyles, ...props }, ref) => {
   );
   if (!Transition) return show ? container : null;
   return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
     <Transition unmountOnExit ref={ref} {...props} in={show}>
       {container}
     </Transition>
