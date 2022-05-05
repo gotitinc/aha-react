7e0010a92d2dbd5c62794e292b2d86ce3c1d6a5b
Thang Tran <uendno@gmail.com>
2022-05-05 09:50:20 +0700

    chore: Deploy using github actions

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
-if [ $CODEBUILD_BUILD_SUCCEEDING -eq 1 ]
-then
   rm -rf es/images
   rm -rf lib/images
   echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
   npm publish --tag $NPM_TAG
-fi
